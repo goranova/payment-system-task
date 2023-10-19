82f31477a613dd2657f45960e4b703cd1a4a9cad
goranova <tsvety_goranova@abv.bg>
2023-10-19 16:29:16 +0300

    creating spring project

---
+++ src/main/java/com/emerchantpay/paymentsystemtask/PaymentSystemTaskApplication.java
@@ -0,0 +1,13 @@
+package com.emerchantpay.paymentsystemtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentSystemTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentSystemTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emerchantpay/paymentsystemtask/PaymentSystemTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emerchantpay.paymentsystemtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentSystemTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
